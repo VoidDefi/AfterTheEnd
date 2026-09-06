e93fa1bb3baca8321e56448c7de626ca6111f51b
defi_ <defi.olen@gmail.com>
2026-09-06 22:22:10 +0300

    Add Cataclysm Texture Replace

--- Reworks/Bosses/CalamitasClone/CataclysmRework.cs
+++ Reworks/Bosses/CalamitasClone/CataclysmRework.cs
@@ -1,16 +1,17 @@
 ﻿using AfterTheEnd.Utilities.Reflection;
 using System;
 using System.Reflection;
-using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
-using CalClone = CalamityMod.NPCs.CalClone.CalamitasClone;
+using Terraria;
+using CalamityMod.NPCs.CalClone;
+using CalamityMod.Items.Accessories;
 
 namespace AfterTheEnd.Reworks.Bosses.CalamitasClone
 {
-    public class CalamitasCloneRework : GlobalNPC
+    public class CataclysmRework : GlobalNPC
     {
-        private static Type ModType = typeof(CalClone);
+        private static Type ModType = typeof(Cataclysm);
 
         private static readonly AutoPropertyInfo TextureInfo = new(ModType, "Texture", BindingFlags.Public | BindingFlags.Instance);
 
@@ -19,16 +20,15 @@ namespace AfterTheEnd.Reworks.Bosses.CalamitasClone
             MonoModHooks.Add(TextureInfo.Value.GetMethod, OnTexture);
         }
 
-        private delegate string OriginGetTexture(CalClone self);
+        private delegate string OriginGetTexture(Cataclysm self);
 
-        private string OnTexture(OriginGetTexture origin, CalClone self)
+        private string OnTexture(OriginGetTexture origin, Cataclysm self)
         {
             if (self.Type == Type)
-                return (GetType().Namespace + "." + "CalamitasClone").Replace('.', '/');
+                return (GetType().Namespace + "." + "Cataclysm").Replace('.', '/');
 
             return origin.Invoke(self);
         }
-
         private int type = 0;
 
         private int Type
@@ -37,10 +37,10 @@ namespace AfterTheEnd.Reworks.Bosses.CalamitasClone
             {
                 if (type < NPCID.Count)
                 {
-                    type = ModContent.NPCType<CalClone>();
+                    type = ModContent.NPCType<Cataclysm>();
                 }
 
-                if (type < NPCID.Count) throw new Exception("Not found cal clone type");
+                if (type < NPCID.Count) throw new Exception("Not found cataclysm type");
 
                 return type;
             }
@@ -50,18 +50,18 @@ namespace AfterTheEnd.Reworks.Bosses.CalamitasClone
 
         public override void SetStaticDefaults()
         {
-            Main.npcFrameCount[Type] = 5;
+            Main.npcFrameCount[Type] = 9;
         }
 
         public override void FindFrame(NPC npc, int frameHeight)
         {
-            if (npc.type == Type) 
+            if (npc.type == Type)
             {
-                npc.frameCounter += 0.15000000596046448 / 1.5;
-                npc.frameCounter %= Main.npcFrameCount[Type] - 1;
+                npc.frameCounter += 0.15000000596046448;
+                npc.frameCounter %= Main.npcFrameCount[Type] - 3;
                 int frame = (int)npc.frameCounter;
                 npc.frame.Y = frame * frameHeight;
-            } 
+            }
         }
     }
 }
